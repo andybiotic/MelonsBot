08485da460426e461a3a2f01b8a24d30d8bcf0e6
Andy <andyandyoakley@me.com>
2021-12-19 20:53:12 +0000

    Basic Michael Masi sassy message.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require("./config.json");
 const client = new Discord.Client();
 
 const botCheckString = "!bot";
+const botTestString = "!test";
 
 const channelBot = "851879147001348119";
 const channelRaceControl = "845976526747074581"
@@ -10,19 +11,7 @@ const channelRaceControl = "845976526747074581"
 const messageBotOperational = "MelonsBot is running!"
 const messageBotStart = "MelonsBot has started..."
 
-const messageRaceControl1 = "Race Control is actively monitoring this channel. When reporting incidents, please remember to include the car number, lap number and any other details you feel are important. Race Control will not comment on incidents, except when a penalty is applied.";
-const messageRaceControl2 = "Please remember that Race Control will not comment on individual incidents, except when a penalty is applied.";
-const messageRaceControl3 = "Fun fact: Melons closest relatives are squashes and cucumbers.";
-const messageRaceControl4 = "The stewards are easily startled. Please do not tap on the glass...";
-
-const messageDict = {
-  0: "Race Control is actively monitoring this channel. When reporting incidents, please remember to include the car number, lap number and any other details you feel are important.",
-  1: "Please remember that Race Control will not comment on individual incidents, except when a penalty is applied.",
-  2: "Fun fact: Melons closest relatives are squashes and cucumbers.",
-  3: "The stewards are easily startled. Please do not tap on the glass...",
-  4: "If we're investigating an incident, we'll add the melon emoji to the message.",
-  5: "Warnings and penalties will be posted in the race-updates channel."
-}
+const messageCount = 8
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -39,7 +28,63 @@ client.on("ready", () => {
 var messageCounter = 0;
 
 client.on("message", function(message) {
-  console.log("MelonsBot: Analysing incoming message.")
+
+  function setSassyString() {
+    let str = ", it's called a motor race. We went car racing."
+    return str;
+  }
+
+  function mergeSassyString(nickname, sassyString) {
+    const str = nickname + sassyString
+    return str;
+  }
+
+  async function setNickname() {
+    try {
+      const member = await message.guild.members.fetch(message.author)
+      return member.nickname;
+    } catch {
+      return message.author.username;
+    }
+  }
+
+  async function postRaceControlMessage () {
+    const messageDict = {
+      0: "Race Control is actively monitoring this channel. When reporting incidents, please remember to include the car number, lap number and any other details you feel are important.",
+      1: "Please remember that Race Control will not comment on individual incidents, except when a penalty is applied.",
+      2: "Fun fact: Melons closest relatives are squashes and cucumbers.",
+      3: "The stewards are easily startled. Please do not tap on the glass...",
+      4: "If we're investigating an incident, we'll add the melon emoji to the message.",
+      5: "Warnings and penalties will be posted in the race-updates channel.",
+      6: "Racers may send diagrams depicting racing incidents to the stewards using the Melons 24h email. These diagrams will be stuck on the refrigerator so we can admire them.",
+      7: "Fun fact: The inaugral Melons 24h event was won by Rust Bucket Racing and Was Cookin Racing Adventures.",
+    }
+    
+    try {
+      const nickname = await setNickname()
+      const sassyString = setSassyString(nickname)
+      client.channels.cache.get(channelBot).send(mergeSassyString(nickname, sassyString));
+    } catch {
+      console.log("Error")
+    }
+  }
+
+  
+
+  function setMessage() {
+    console.log("7")
+    const testPromise = async () => {
+      console.log("8")
+  
+      
+      console.log("9");
+      return messageDict;
+
+      client.channels.cache.get(channelBot).send(messageDict[8]);
+    }
+  }
+
+  console.log("MelonsBot: Analysing incoming message.");
 
   if (message.author.bot) {
     console.log("MelonsBot: Message was sent by bot. Discarding.");
@@ -52,16 +97,22 @@ client.on("message", function(message) {
       console.log("MelonsBot: Bot check.");
       client.channels.cache.get(channelBot).send(messageBotOperational);
       return
+    } else if (message.content.startsWith(botTestString)) {
+      console.log("1")
+      // DO NEW STUFF
+      postRaceControlMessage()
+      
     }
     return
   }
 
   if (message.channel.id === channelRaceControl) {
     console.log("MelonsBot: Message sent by user in Race Control channel.");
+    lastMessageAuthor = "Test Author";
     messageCounter += 1;
 
     if (messageCounter % 5 === 0) {
-      client.channels.cache.get(channelRaceControl).send(messageDict[getRandomInt(6)]);
+      client.channels.cache.get(channelRaceControl).send(messageDict[getRandomInt(messageCount)]);
       console.log("MelonsBot: Sent race control message.");
     }
   }
