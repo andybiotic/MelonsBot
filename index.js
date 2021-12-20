45b83af6a11037662fcca01b479db5cede46b5bd
Andy <andyandyoakley@me.com>
2021-12-20 17:41:55 +0000

    Added advert images

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const messageBotOperational = "MelonsBot is running!"
 const messageBotStart = "MelonsBot has started..."
 
 const totalMessageCount = 8
-const totalAdvertCount = 2
+const totalAdvertCount = 15
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
