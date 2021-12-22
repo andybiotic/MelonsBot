954d9e6f054de2668c795a95bc0d20b9696f8ce7
Andy <andyandyoakley@me.com>
2021-12-22 21:51:51 +0000

    Added some images

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ client.on("message", function(message) {
       3: "The stewards are easily startled. Please do not tap on the glass...",
       4: "If we're investigating an incident, we'll add the melon emoji to the message.",
       5: "Warnings and penalties will be posted in the race-updates channel.",
-      6: "Racers may send diagrams depicting racing incidents to the stewards using the Melons 24h email. These diagrams will be stuck on the refrigerator so we can admire them.",
+      6: "Fun fact: The 'D' in Andy D Oakley stands for 'Divebomb'.",
       7: "Fun fact: The inaugral Melons 24h event was won by Rust Bucket Racing and Was Cookin Racing Adventures.",
     }
     
