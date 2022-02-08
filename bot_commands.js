f607b79765999b31bf6201fafb9bafbf066ff4b4
Andy Oakley <andyandyoakley@me.com>
2022-02-08 10:45:20 +0000

    Changed startup message.

--- bot_commands.js
+++ bot_commands.js
@@ -7,25 +7,25 @@ const botReminderString = "!remind";
 const botPaddockString = "!paddock";
 const botSwitchModeString = "!mode";
 
+const startupMessage = 'MelonsBot is running. Type !help for commands and information.'
 const helpInformation = `
-MelonsBot is running.
+*You can control MelonsBot here in the bot channel, using the commands listed below.*
 
-*You can communicate with MelonsBot here in the bot channel, using the commands listed below.*
-
-**Testing and Debugging:**
+**Testing and debugging:**
 *${botCheckString}* -- Check if MelonsBot is active.
-*${botTestString}* -- Send a specific message to test to the bot channel. 
+*${botTestString}* -- Send a specific test message to the bot channel. 
 *${botRandomString}* -- Send a random message to the bot channel.
 *${botHelpString}* -- Repeat this message in the bot channel.
 *${botSwitchModeString}* -- Switch from Test Mode to Race Mode.
 
+In Test Mode, all adverts and paddock messages are sent to the bot channel for testing. In Race Mode, messages are sent to the the paddock channel.
 
-**Send a public message:**
+**Send a server message:**
 *${botAdvertString}* -- Post an advert in sequence to the paddock channel. Loops when all adverts have been posted.
 *${botReminderString}* -- Post a reminder to members in the paddock channel.
 *${botPaddockString}* -- Post a fun message to the paddock. Loops when all messages have been posted.
 
-*MelonsBot will also reply periodically in the race control channel with a random helpful message.*
+*MelonsBot will also post randomly (after every 5 messages) in the race control channel with a random helpful message.*
 `
 
 const reminderInformation = `
@@ -41,6 +41,7 @@ module.exports = {
     botReminderString, 
     botPaddockString,
     botSwitchModeString,
+    startupMessage,
     helpInformation, 
     reminderInformation,
 }
